bf28af58570d261969f1d8751b2a8aea0aca665f
DongHwan Seo <61683387+SNMac@users.noreply.github.com>
2022-09-11 19:54:56 +0900

    Add files via upload

---
+++ 15829.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+#define M 1234567891
+
+using namespace std;
+
+long long hashing(int l, const string& str) {
+    long long result = 0;
+    long long r = 1;
+    for (int i = 0; i < l; i++) {
+        // (a + b) % n = {(a % n) + (b % n)} % n
+        result = (result + ((str[i] - 'a' + 1) * r) % M) % M;
+        r = (r * 31) % M;
+    }
+    return result;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int l;
+    string str;
+    cin >> l;
+    cin >> str;
+    cout << hashing(l, str) << '\n';
+}
