4dfbf35c42fd49031d3d687d2fc410b500ef1db1
DongHwan Seo <61683387+SNMac@users.noreply.github.com>
2022-09-12 11:16:44 +0900

    Add files via upload

---
+++ 11866 - queue.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    queue<int> q;
+    int n, k;
+    cin >> n >> k;
+    for (int i = 1; i <= n; i++)
+        q.push(i);
+
+    cout << '<';
+    while(true) {
+        for (int i = 1; i < k; i++) {
+            q.push(q.front());
+            q.pop();
+        }
+        cout << q.front();
+        q.pop();
+        if (q.empty()) {
+            cout << '>';
+            return 0;
+        } else {
+            cout << ", ";
+        }
+    }
+}
