701f7d073317a61c012759ffc2140bb0f1bc7bbe
DongHwan Seo <61683387+SNMac@users.noreply.github.com>
2023-03-06 16:58:21 +0900

    Add files via upload

---
+++ 10812 - rotate().cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+    vector<int> v;
+
+    for (int a = 1; a <= n; a++)
+        v.push_back(a);
+
+    for (int a = 0; a < m; a++) {
+        int i, j, k;
+        cin >> i >> j >> k;
+        rotate(v.begin() + (i - 1), v.begin() + (k - 1), v.begin() + j);
+    }
+
+    for (int it : v)
+        cout << it << ' ';
+    cout << '\n';
+}
