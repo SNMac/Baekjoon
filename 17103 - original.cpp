119197bc28fe885dbf61852e0ca581d3bdfe4c25
DongHwan Seo <61683387+SNMac@users.noreply.github.com>
2023-09-29 14:45:40 +0900

    Add files via upload

---
+++ 17103 - original.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+
+#define MAX 1000001
+using namespace std;
+
+bool prime[MAX];
+
+void primeInit() {
+    fill_n(prime, MAX, true);
+    prime[0] = prime[1] = false;
+    for (int i = 2; i < MAX; i++) {
+        if (!prime[i])
+            continue;
+
+        for (int j = 2; j * j <= i; j++) {  // 소수 판별
+            if (i % j == 0) {
+                prime[i] = false;
+                break;
+            }
+        }
+        if (prime[i]) {
+            for (int j = i + i; j < MAX; j += i)
+                prime[j] = false;
+        }
+    }
+}
+
+int countGold(int n) {
+    int total = 0;
+    for (int i = 2; i <= n / 2; i++) {
+        if (prime[i] & prime[n - i])
+            total++;
+    }
+    return total;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    primeInit();
+    int t;
+    cin >> t;
+    for (int i = 0; i < t; i++) {
+        int n;
+        cin >> n;
+        cout << countGold(n) << '\n';
+    }
+}
